ab426e91baf269b383c4b8f524b38485cad9d080
riaanjlagrange <riaanjlagrange@gmail.com>
2025-08-20 12:13:28 +0200

    minor UI fixes / refactors

--- lib/pages/videos/videos_detail_page.dart
+++ lib/pages/videos/videos_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nws/models/videos_categories_model.dart';
 import 'package:nws/models/videos_model.dart';
 
 class VideosDetailPage extends StatelessWidget {
@@ -10,20 +11,35 @@ class VideosDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(video.title)),
-      body: Column(
-        children: [
-          Image.network(video.thumbnailUrl),
-          const SizedBox(height: 20),
-          Text(
-            video.title,
-            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            video.category.toString(),
-            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-        ],
+      body: Padding(
+        padding: EdgeInsetsGeometry.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20),
+            // TODO: replace thumnail with actual video
+            Container(
+              width: double.infinity,
+              child: Center(child: Image.network(video.thumbnailUrl)),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              video.title,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Chip(
+              label: Text(
+                video.category.name,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              backgroundColor: video.category.color,
+              labelStyle: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/pages/videos/videos_page.dart
+++ lib/pages/videos/videos_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nws/components/videos_card.dart';
+import 'package:nws/pages/videos/videos_card.dart';
 import 'package:nws/models/videos_categories_model.dart';
 import 'package:nws/models/videos_model.dart';
 
@@ -13,19 +13,60 @@ class VideosPage extends StatefulWidget {
 }
 
 class _VideosPageState extends State<VideosPage> {
+  // list of placeholder videos until firebase storage integration
   final List<VideosModel> videos = [
     VideosModel(
       id: '1',
-      title: 'Beginner Workout',
-      thumbnailUrl: 'https://via.placeholder.com/300x180.png?text=Workout+1',
+      title: 'Basics sample 1',
+      thumbnailUrl: 'https://placehold.co/300x180.png',
       videoUrl: 'https://example.com/workout1.mp4',
       category: VideosCategories.basic,
       createdAt: DateTime.now(),
     ),
     VideosModel(
       id: '2',
-      title: 'Flutter Tutorial',
-      thumbnailUrl: 'https://via.placeholder.com/300x180.png?text=Tutorial+1',
+      title: 'Basics sample 2',
+      thumbnailUrl: 'https://placehold.co/300x180.png',
+      videoUrl: 'https://example.com/tutorial1.mp4',
+      category: VideosCategories.basic,
+      createdAt: DateTime.now(),
+    ),
+    VideosModel(
+      id: '3',
+      title: 'Intermediate sample 1',
+      thumbnailUrl: 'https://placehold.co/300x180.png',
+      videoUrl: 'https://example.com/tutorial1.mp4',
+      category: VideosCategories.intermediate,
+      createdAt: DateTime.now(),
+    ),
+    VideosModel(
+      id: '4',
+      title: 'Intermediate sample 2',
+      thumbnailUrl: 'https://placehold.co/300x180.png',
+      videoUrl: 'https://example.com/tutorial1.mp4',
+      category: VideosCategories.intermediate,
+      createdAt: DateTime.now(),
+    ),
+    VideosModel(
+      id: '5',
+      title: 'Advanced sample 1',
+      thumbnailUrl: 'https://placehold.co/300x180.png',
+      videoUrl: 'https://example.com/tutorial1.mp4',
+      category: VideosCategories.advanced,
+      createdAt: DateTime.now(),
+    ),
+    VideosModel(
+      id: '6',
+      title: 'Advanced sample 2',
+      thumbnailUrl: 'https://placehold.co/300x180.png',
+      videoUrl: 'https://example.com/tutorial1.mp4',
+      category: VideosCategories.advanced,
+      createdAt: DateTime.now(),
+    ),
+    VideosModel(
+      id: '7',
+      title: 'Advanced sample 3',
+      thumbnailUrl: 'https://placehold.co/300x180.png',
       videoUrl: 'https://example.com/tutorial1.mp4',
       category: VideosCategories.advanced,
       createdAt: DateTime.now(),
